0fec154c25e224a58b7855027e4dd49661d73942
sukith_narala <74507039+sukith89@users.noreply.github.com>
2021-06-06 17:05:51 +0530

    Update home_service.sh

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -2,42 +2,37 @@
 
 terminator -e "
 	cd $(pwd)/../..;
-    source devel/setup.bash;
-    export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/sukithOffice.world;
-    roslaunch turtlebot_gazebo turtlebot_world.launch" &
+    	source devel/setup.bash;
+    	export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/simple_home.world;
+    	roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 15
 
 terminator -e "
 	cd $(pwd)/../..;
-    source devel/setup.bash;
-    export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../map/smap.yaml;
-    roslaunch turtlebot_gazebo amcl_demo.launch " &
+    	source devel/setup.bash;
+    	export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../map/smap.yaml;
+    	roslaunch turtlebot_gazebo amcl_demo.launch " &
 
 sleep 5
 
 terminator -e "
 	cd $(pwd)/../..;
 	source devel/setup.bash;
-    rosrun rviz rviz -d $(pwd)/../rvizConfig/home_service.rviz" &
+    	rosrun rviz rviz -d $(pwd)/../rvizConfig/home_service.rviz" &
 
 sleep 20
 
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    rostopic echo /object_state" &
-
-terminator -e "
-	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    rosrun add_markers add_markers" &
+    	source devel/setup.bash; 
+    	rosrun add_markers add_markers" &
 
 sleep 1
 
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    rosrun pick_objects pick_objects"
+    	source devel/setup.bash; 
+    	rosrun pick_objects pick_objects"
 
 
