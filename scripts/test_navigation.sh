768c6e470d7c70f28fac6b105e5941705addf5e9
sukith_narala <74507039+sukith89@users.noreply.github.com>
2021-06-06 17:02:46 +0530

    Update test_navigation.sh

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -3,21 +3,21 @@
 
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../../src/map/sukithOffice.world;  
-    roslaunch turtlebot_gazebo turtlebot_world.launch" &
+    	source devel/setup.bash; 
+    	export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/simple_home.world;  
+	roslaunch turtlebot_gazebo turtlebot_world.launch" &
     
 sleep 10
    
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../map/smap.yaml;
-    roslaunch turtlebot_gazebo amcl_demo.launch " &
+   	source devel/setup.bash; 
+    	export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../map/smap.yaml;
+   	roslaunch turtlebot_gazebo amcl_demo.launch " &
 
 sleep 3
 
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    roslaunch turtlebot_rviz_launchers view_navigation.launch"
+    	source devel/setup.bash; 
+    	roslaunch turtlebot_rviz_launchers view_navigation.launch"
