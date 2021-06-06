399bb6f383c15f3c41a13950eea31631b0f863e7
sukith_narala <74507039+sukith89@users.noreply.github.com>
2021-06-06 17:06:58 +0530

    Update add_marker.sh

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -2,29 +2,29 @@
 
 terminator -e "
 	cd $(pwd)/../..;
-    source devel/setup.bash;
-    export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/simple_home.world;
-    roslaunch turtlebot_gazebo turtlebot_world.launch" &
+    	source devel/setup.bash;
+    	export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/../map/simple_home.world;
+    	roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 15
 
 terminator -e "
 	cd $(pwd)/../..;
-    source devel/setup.bash;
-    export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../../src/map/smap.yaml;
-    roslaunch turtlebot_gazebo amcl_demo.launch " &
+    	source devel/setup.bash;
+    	export TURTLEBOT_GAZEBO_MAP_FILE=$(pwd)/../map/smap.yaml;
+    	roslaunch turtlebot_gazebo amcl_demo.launch " &
 
 sleep 5
 
 terminator -e "
 	cd $(pwd)/../..;
 	source devel/setup.bash;
-    roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+    	roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 
 sleep 20
 
 
 terminator -e "
 	cd $(pwd)/../..; 
-    source devel/setup.bash; 
-    rosrun add_markers_test add_markers_test"
+    	source devel/setup.bash; 
+    	rosrun add_markers_test add_markers_test"
